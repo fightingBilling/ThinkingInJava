7906eeddb05ac33c879773df7a74419e547fce57
wangzhengyi <wzyll1314@gmail.com>
2014-07-07 16:37:57 +0800

    完成练习25

---
+++ src/chapter21/Exercise25.java
@@ -0,0 +1,7 @@
+package chapter21;
+
+public class Exercise25 {
+    public static void main(String[] args) {
+        System.out.println("Order up! and Chef interrupted will not appear");
+    }
+}
